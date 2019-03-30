8331e249ce80a2d155634d757d7bd4f71e726ad1
hegemonies <hegemonies@yandex.ru>
2019-03-30 08:20:46 +0700

    Add function to work with BD

--- src/main/java/BravoCI/Core.java
+++ src/main/java/BravoCI/Core.java
@@ -1,7 +1,17 @@
 package BravoCI;
 
+//public class Core {
+//    public static void main(String... args) {
+//        System.out.println("hello world");
+//    }
+//}
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Core {
     public static void main(String... args) {
-        System.out.println("hello world");
+        SpringApplication.run(Core.class);
     }
 }
--- src/main/java/BravoCI/Frontend/MainController.java
+++ src/main/java/BravoCI/Frontend/MainController.java
@@ -24,8 +24,34 @@ public class MainController {
         return "OK";
     }
 
+    @RequestMapping("/addCommit")
+    public String addCommit(@RequestParam(name = "Name", required = true) String Name,
+                            @RequestParam(name = "Commit", required = true) String Commit) {
+        User user = search(Name);
+
+        if (user == null) {
+            return "bad";
+        }
+
+        user.addCommit(new CommitInfo(Commit, null, false));
+
+        userRepository.save(user);
+
+        return "good";
+    }
+
     @RequestMapping("/showAll")
     public List<User> showAll() {
         return userRepository.findAll();
+    }
+
+    private User search(String name) {
+        for (User user : userRepository.findAll()) {
+            if (user.getName().equals(name)) {
+                return user;
+            }
+        }
+
+        return null;
     }
 }
