1d0ee8b0a34805a36f1fd061be4fa7cff314b9b1
hegemonies <hegemonies@yandex.ru>
2019-03-30 08:21:35 +0700

    Fix Core.main()

--- src/main/java/BravoCI/Core.java
+++ src/main/java/BravoCI/Core.java
@@ -1,11 +1,5 @@
 package BravoCI;
 
-//public class Core {
-//    public static void main(String... args) {
-//        System.out.println("hello world");
-//    }
-//}
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
