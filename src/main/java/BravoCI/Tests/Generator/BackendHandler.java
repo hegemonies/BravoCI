7bd4fd3b3ceaae5497e7e0de381ccc7defb22448
hegemonies <hegemonies@yandex.ru>
2019-04-04 01:53:06 +0700

    Fix BackendHandler (work on my machine)

--- src/main/java/BravoCI/Tests/Generator/BackendHandler.java
+++ src/main/java/BravoCI/Tests/Generator/BackendHandler.java
@@ -7,35 +7,51 @@ import com.github.dockerjava.api.model.Bind;
 import com.github.dockerjava.api.model.Container;
 import com.github.dockerjava.api.model.Image;
 import com.github.dockerjava.core.DockerClientBuilder;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+
+import static com.github.dockerjava.api.model.HostConfig.newHostConfig;
 
 public class BackendHandler implements Runnable {
-    DockerClient dockerClient = DockerClientBuilder.getInstance().build();
-    String pathLocaleFolder = "/repos";
-    String username;
-    String repo;
-    File folderLocale;
-    File userFolder;
-    String localeVolume;
-    String shareVolume;
+    private final DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+    private final String pathLocaleFolder = "/home/dan/repos";
+    private final String pathLogs = "/home/dan/results";
+    private String username;
+    private String repo;
+    private File folderLocale;
+    private File userFolder;
+    private File logsFolder;
+    private File userLogsFolder;
+    private String localeVolume;
+    private String shareVolume;
 
     @Override
     public void run() {
         String request;
         while (true) {
             request = WrapperQueue.getFromQueue();
-            username = request.split(":")[0];
-            repo = request.split(":")[1];
 
             if (!request.equals("EMPTY")) {
+                username = request.split(":")[0];
+                repo = request.split(":")[1];
+
+                System.out.println("thread [" + Thread.currentThread().getId() + "] getting task for "
+                        + username + " " + repo);
+
                 this.prepareToTesting();
                 this.startTesting();
                 this.afterTesting();
+
+                System.out.println("thread [" + Thread.currentThread().getId() + "] finish task for "
+                        + username + " " + repo);
             } else {
                 try {
                     Thread.sleep(10000);
@@ -47,6 +63,7 @@ public class BackendHandler implements Runnable {
     }
 
     private void prepareToTesting() {
+        // set up folders
         folderLocale = new File(pathLocaleFolder);
         if (!folderLocale.exists()) {
             folderLocale.mkdir();
@@ -57,13 +74,36 @@ public class BackendHandler implements Runnable {
             userFolder.mkdir();
         }
 
+        logsFolder = new File(pathLogs);
+        if (!logsFolder.exists()) {
+            logsFolder.mkdir();
+        }
+
+        userLogsFolder = new File(pathLogs + "/" + username);
+        if (!userLogsFolder.exists()) {
+            userLogsFolder.mkdir();
+        }
+
         localeVolume = pathLocaleFolder + "/" + username + "/" + repo;
         shareVolume = localeVolume +":/media";
 
-        // todo need clone repo from github using scripts
+        // cloning repo from github
+        String url_repo = "https://github.com/" + username + "/" + repo;
+
+        ProcessBuilder processBuilder = new ProcessBuilder("./clone_repo.sh",
+                url_repo,
+                pathLocaleFolder + "/" + username);
+
+        try {
+            Process process = processBuilder.start();
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private void startTesting() {
+        // generate bravo.sh from bravo.json
         Content content = null;
         try {
             content = Generator.readJSON(localeVolume);
@@ -72,51 +112,67 @@ public class BackendHandler implements Runnable {
             e.printStackTrace();
         }
 
-        String compiler = content.getConfig().getCompiler();
+        String compiler = "bravo_" + Objects.requireNonNull(content).getConfig().getCompiler();
+
+        // running tests inside the docker container
 
         boolean imageExist = false;
         String imageTag = null;
         List<Image> images = dockerClient.listImagesCmd()
                 .withImageNameFilter(compiler)
-                .exec(); // todo: check for performance
+                .exec();
 
-        for (Image image : images) { // todo need refactor
-            if (image.getRepoTags().equals(compiler)) {
-                imageExist = true;
-                imageTag = image.getRepoTags()[0];
+        for (Image image : images) {
+            String[] imageTags = image.getRepoTags();
+
+            if (imageTags != null) {
+                for (String cur_imageTag : imageTags) {
+                    if (cur_imageTag.equals(compiler + ":latest")) {
+                        imageExist = true;
+                        imageTag = cur_imageTag;
+                    }
+                }
             }
         }
 
         if (imageExist) {
+            // create a running container with bind mounts
             CreateContainerResponse container = dockerClient.createContainerCmd(imageTag)
-                    .withBinds(Bind.parse(shareVolume))
+                    .withHostConfig(newHostConfig().withBinds(Bind.parse(shareVolume)))
                     .exec();
             dockerClient.startContainerCmd(container.getId()).exec();
 
-            List<Container> exited_containers = dockerClient.listContainersCmd()
-                    .withShowAll(true)
-                    .withStatusFilter("exited")
-                    .exec();
-
             boolean containerFinish = false;
             while (!containerFinish) {
-                for (Container _container : exited_containers) {
-                    if (_container.getId().equals(container.getId())) {
-                        containerFinish = true;
-                    }
-                }
-
                 try {
                     Thread.sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            }
 
-            File logs = new File(localeVolume + "/logs.txt");
-            logs.renameTo(new File("/results/" + username + "/" + repo + "/logs.txt"));
+                List<Container> exited_containers = dockerClient.listContainersCmd()
+                        .withShowAll(true)
+                        .withStatusFilter(Arrays.asList("exited"))
+                        .exec();
+
+                for (Container _container : exited_containers) {
+                    if (_container.getId().equals(container.getId())) {
+                        containerFinish = true;
+                    }
+                }
+            }
         } else {
             writeToLog("The necessary image does not exist");
+        }
+
+        File logs = new File(localeVolume + "/logs.txt");
+        logs.renameTo(new File(pathLogs + "/" + username + "/" + repo + "/logs.txt"));
+
+        // delete repo
+        try {
+            FileUtils.deleteDirectory(new File(localeVolume));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -125,6 +181,10 @@ public class BackendHandler implements Runnable {
         repo = null;
         folderLocale = null;
         userFolder = null;
+        logsFolder = null;
+        userLogsFolder = null;
+        localeVolume = null;
+        shareVolume = null;
     }
 
     private void writeToLog(String string) {
